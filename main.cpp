b70e4c74a63ff0f55c9a2cd3e6b621c3bc5ea027
Coding-Ocean <eijo524@gmail.com>
2024-03-03 15:15:01 +0900

    functor

--- main.cpp
+++ main.cpp
@@ -3,12 +3,15 @@
 #include<algorithm>
 using namespace std;
 
-//関数
-bool comp(int a, int b) { return a > b; }
+//関数オブジェクト
+class comp {
+public:
+	bool operator()(int a, int b) { return a > b; }
+};
 
 int main() {
 	vector<int> v{ 5,2,4,3,1 };
-	sort(v.begin(), v.end(), comp);
+	sort(v.begin(), v.end(), comp());
 	for (const auto& e : v)cout << e << endl;
 	system("pause>0");
 }
