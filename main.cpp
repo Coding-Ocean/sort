0ad8be9948a679020ba08308bbb9b558516b0692
Coding-Ocean <eijo524@gmail.com>
2024-03-03 15:22:22 +0900

    lambda

--- main.cpp
+++ main.cpp
@@ -3,15 +3,11 @@
 #include<algorithm>
 using namespace std;
 
-//関数オブジェクト
-class comp {
-public:
-	bool operator()(int a, int b) { return a > b; }
-};
-
 int main() {
 	vector<int> v{ 5,2,4,3,1 };
-	sort(v.begin(), v.end(), comp());
+	//ラムダ式
+	auto comp = [](int a, int b) {return a < b; };
+	sort(v.begin(), v.end(), comp );
 	for (const auto& e : v)cout << e << endl;
 	system("pause>0");
 }
