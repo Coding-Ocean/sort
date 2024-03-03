c4c7773ed2559a088606f1a8722ec09ecdfbacda
Coding-Ocean <eijo524@gmail.com>
2024-03-03 15:10:49 +0900

    function

--- main.cpp
+++ main.cpp
@@ -2,9 +2,13 @@
 #include<vector>
 #include<algorithm>
 using namespace std;
+
+//ŠÖ”
+bool comp(int a, int b) { return a > b; }
+
 int main() {
 	vector<int> v{ 5,2,4,3,1 };
-	sort(v.begin(), v.end(), greater<>());
+	sort(v.begin(), v.end(), comp);
 	for (const auto& e : v)cout << e << endl;
 	system("pause>0");
 }
