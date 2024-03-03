a6ebe045848268a180fe30d52cb1cfdc223f8361
Coding-Ocean <eijo524@gmail.com>
2024-03-03 15:36:33 +0900

    struct

--- main.cpp
+++ main.cpp
@@ -3,11 +3,28 @@
 #include<algorithm>
 using namespace std;
 
+struct DATA {
+	string name;
+	int num;
+};
+struct comp_data_name {
+	bool operator()(DATA& a, DATA& b) {
+		return a.name < b.name;
+	}
+};
+struct comp_data_num {
+	bool operator()(DATA& a, DATA& b) {
+		return a.num < b.num;
+	}
+};
 int main() {
-	vector<int> v{ 5,2,4,3,1 };
-	//ƒ‰ƒ€ƒ_Ž®
-	auto comp = [](int a, int b) {return a < b; };
-	sort(v.begin(), v.end(), comp );
-	for (const auto& e : v)cout << e << endl;
+	vector<DATA> v{
+		{"HKT",48},
+		{"Nogizaka",46},
+		{"AKB",48},
+		{"Sakurazaka",46}, 
+	};
+	sort(v.begin(), v.end(), comp_data_num() );
+	for (const auto& e : v)cout << e.name << e.num << endl;
 	system("pause>0");
 }
