3fe4711733e848c65ea4bac313807b6759d13213
Coding-Ocean <eijo524@gmail.com>
2024-03-04 10:53:08 +0900

    add comment

--- main.cpp
+++ main.cpp
@@ -3,15 +3,19 @@
 #include<algorithm>
 using namespace std;
 
-//関数オブジェクト
+//関数オブジェクト(ファンクタ)
 class comp {
 public:
 	bool operator()(int a, int b) { return a > b; }
 };
 
 int main() {
+	//データを入れてｖを用意
 	vector<int> v{ 5,2,4,3,1 };
+	//ファンクタをsortに渡して並び変え
 	sort(v.begin(), v.end(), comp());
+	//全要素表示
 	for (const auto& e : v)cout << e << endl;
+
 	system("pause>0");
 }
