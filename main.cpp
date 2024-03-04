80509e01bbc15cc453acb29f93ac43d72093b48a
Coding-Ocean <eijo524@gmail.com>
2024-03-04 10:47:07 +0900

    add comment

--- main.cpp
+++ main.cpp
@@ -2,9 +2,17 @@
 #include<vector>
 #include<algorithm>
 using namespace std;
+
+//ベクター配列を普通にソートする
 int main() {
+	//データを入れたｖを用意する
 	vector<int> v{ 5,2,4,3,1 };
+	//大きい順にソート
 	sort(v.begin(), v.end(), greater<>());
+	//ちなみに小さい順はこれだけでよい
+	//sort(v.begin(), v.end());
+	//全要素表示
 	for (const auto& e : v)cout << e << endl;
+
 	system("pause>0");
 }
