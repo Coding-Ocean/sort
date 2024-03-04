f7cb9596978c18ae57e61685bcd6730880dd4bcf
Coding-Ocean <eijo524@gmail.com>
2024-03-04 10:55:12 +0900

    add comment

--- main.cpp
+++ main.cpp
@@ -4,10 +4,14 @@
 using namespace std;
 
 int main() {
+	//データを入れてｖを用意
 	vector<int> v{ 5,2,4,3,1 };
 	//ラムダ式
 	auto comp = [](int a, int b) {return a < b; };
+	//ラムダ式をsortに渡して並び変え
 	sort(v.begin(), v.end(), comp );
+	//全要素表示
 	for (const auto& e : v)cout << e << endl;
+
 	system("pause>0");
 }
