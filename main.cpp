46fe1c316758e33fe8039889fa7754c07d84c84e
Coding-Ocean <eijo524@gmail.com>
2024-03-04 11:02:41 +0900

    add comment

--- main.cpp
+++ main.cpp
@@ -2,29 +2,40 @@
 #include<vector>
 #include<algorithm>
 using namespace std;
-
+//構造体型
 struct DATA {
 	string name;
 	int num;
 };
+//nameの小さい順にソートするときのファンクタ
 struct comp_data_name {
 	bool operator()(DATA& a, DATA& b) {
 		return a.name < b.name;
 	}
 };
+//numの小さい順にソートするときのファンクタ
 struct comp_data_num {
 	bool operator()(DATA& a, DATA& b) {
 		return a.num < b.num;
 	}
 };
+//構造体データをソートする
 int main() {
+	//データを入れてｖを用意
 	vector<DATA> v{
 		{"HKT",48},
 		{"Nogizaka",46},
 		{"AKB",48},
 		{"Sakurazaka",46}, 
 	};
-	sort(v.begin(), v.end(), comp_data_num() );
+
+	//nameの小さい順にソート
+	sort(v.begin(), v.end(), comp_data_name());
+	//numの小さい順にソート
+	//sort(v.begin(), v.end(), comp_data_num());
+
+	//全要素表示
 	for (const auto& e : v)cout << e.name << e.num << endl;
+
 	system("pause>0");
 }
